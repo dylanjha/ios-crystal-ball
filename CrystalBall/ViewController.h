4f0919fed2d70a3f52d702749823b86ddbe0221c
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-30 17:31:20 -0700

    Initial Commit

---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Dylan on 9/30/13.
+//  Copyright (c) 2013 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CrystalBall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Dylan on 9/30/13.
+//  Copyright (c) 2013 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
