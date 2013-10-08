707392825e87021fa8a6cbd2052a3f788f6f8dff
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-10-08 00:33:49 -0400

    background imaage ImageView

--- CrystalBall/ViewController.m
+++ CrystalBall/ViewController.m
@@ -31,7 +31,15 @@
 
 - (IBAction)buttonPressed:(UIButton *)sender {
     NSLog(@"button pressed");
-    NSUInteger randomIndex = arc4random() % [self.predictionArray count];
+//    NSUInteger randomIndex = arc4random() % self.predictionArray.count;
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *randomColor = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    
+    
+    NSUInteger randomIndex = arc4random_uniform(self.predictionArray.count);
+    self.predictionLabel.backgroundColor = randomColor;
     self.predictionLabel.text = [self.predictionArray objectAtIndex:randomIndex];
 }
 @end
